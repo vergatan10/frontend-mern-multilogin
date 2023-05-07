5873ca54291981f658f395b4ba754c6fc14f29dd
Verga Tandika <64019419+vergatan10@users.noreply.github.com>
2023-05-07 12:22:49 +0700

    deploy

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const ProductList = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const response = await axios.get("http://localhost:5000/products");
+    setProducts(response.data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const deleteProducts = async (productId) => {
+    await axios.delete(`http://localhost:5000/products/${productId}`);
+    getProducts();
+  };
+
+  return (
+    <div>
+      <h1 className="title">Products</h1>
+      <h2 className="subtitle">List Of Products</h2>
+      <Link to="/products/add" className="button is-primary mb-2">
+        Add New
+      </Link>
+      <table className="table is-striped is-fullwidth">
+        <thead>
+          <tr>
+            <th>No</th>
+            <th>Product Name</th>
+            <th>Price</th>
+            <th>Created By</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, index) => (
+            <tr key={product.uuid}>
+              <td>{index + 1}</td>
+              <td>{product.name}</td>
+              <td>Rp. {product.price}</td>
+              <td>{product.user.name}</td>
+              <td>
+                <Link
+                  to={`/products/edit/${product.uuid}`}
+                  className="button is-small is-info"
+                >
+                  Edit
+                </Link>
+                <button
+                  onClick={() => deleteProducts(product.uuid)}
+                  className="button is-small is-danger"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import "bulma/css/bulma.css";
+import axios from "axios";
 
-const container = document.getElementById('root');
+axios.defaults.withCredentials = true;
+
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
@@ -16,8 +18,3 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
